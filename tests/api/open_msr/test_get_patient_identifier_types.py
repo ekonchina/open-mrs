664e26930d8a168661097a9c164094ff8e9c386b
ekonchina <ekaterina-konchina@yandex.ru>
2026-02-03 16:21:07 +0300

    fix

--- tests/api/open_msr/test_get_patient_identifier_types.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.mark.api
-class TestPatientIdentifierTyped:
-    def test_patient_identifier_types(self, api_manager):
-        types = api_manager.admin_steps.get_patient_identifier_types()
-        assert types.results
--- tests/api/open_msr/test_get_roles.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-@pytest.mark.api
-class TestGetRoles:
-    def test_get_roles(self, api_manager):
-        roles = api_manager.admin_steps.get_roles()
-        assert roles.results
