8435fcc3391f7f659c2da2de7b764b69fab162d0
ekonchina <ekaterina-konchina@yandex.ru>
2026-02-03 16:19:59 +0300

    fix

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -9,22 +9,6 @@ from src.api.models.responses.create_person_response import CreatPersonResponse
 
 
 
-def cleanup_object(objects: list):
-    api_manager = ApiManager(objects)
-    for obj in objects:
-        if isinstance(obj, CreatPersonResponse):
-            api_manager.admin_steps.delete_person(obj.uuid, purge=True)
-        elif isinstance(obj, PatientCreateResponse):
-            api_manager.admin_steps.delete_patient(obj.uuid, purge=True)
-        else:
-            logging.warning(f'Object type: {type(obj)} is not deleted')
-
-
-@pytest.fixture
-def created_objects():
-    objects: list = []
-    yield objects
-    cleanup_object(objects)
 
 
 
